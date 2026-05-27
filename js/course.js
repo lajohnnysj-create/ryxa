86f52c0310f0b058dbdf2edfd9a4849fc5b818a6
lajohnnysj-create <la.johnny.sj@gmail.com>
2026-05-27 00:52:48 -0700

    Add files via upload

--- js/course.js
+++ js/course.js
@@ -2516,7 +2516,6 @@ function setupImageSizing(quill, container, saveCallback) {
   group.innerHTML = '<button type="button" data-img-size="small" title="Small image" aria-label="Small image size">S</button>'
     + '<button type="button" data-img-size="medium" title="Medium image" aria-label="Medium image size">M</button>'
     + '<button type="button" data-img-size="large" title="Large image" aria-label="Large image size">L</button>'
-    + '<span class="lesson-img-toolbar-sep" aria-hidden="true"></span>'
     + '<button type="button" data-img-action="remove" title="Remove image" aria-label="Remove image" class="lesson-img-remove-btn">'
     + '<svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>'
     + '</button>';
