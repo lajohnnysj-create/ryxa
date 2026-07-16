f62ce45f884ada92e82b88941c0faf61ef563e9d
lajohnnysj-create <la.johnny.sj@gmail.com>
2026-07-15 23:29:16 -0700

    Add files via upload

--- js/dashboard-shell.js
+++ js/dashboard-shell.js
@@ -2202,7 +2202,7 @@ function updateTierUI() {
       planNavLabel.textContent = 'Plans';
       planNavBtn.setAttribute('aria-label', 'Plans');
       if (planNavIcon) {
-        planNavIcon.innerHTML = '<path d="M12 2l2.5 6.5L21 9l-5 4.5L17.5 21 12 17l-5.5 4L8 13.5 3 9l6.5-.5z"/>';
+        planNavIcon.innerHTML = '<path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91 0z"/><path d="M12 15l-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"/><path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0"/><path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5"/>';
       }
     }
   }
